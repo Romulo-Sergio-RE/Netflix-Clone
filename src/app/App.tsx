11597b4fce1c5438378d7f6030724542b3844568
Romulo-Sergio-RE <89258510+Romulo-Sergio-RE@users.noreply.github.com>
2022-10-04 17:14:38 -0300

    consumindo api de filmes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from './components/header';
 
 function App() {
+  const [filmes, setFilmes] = useState([])
+  const api_key = "8426a1374a213b578abf04fbd0c08f8a"
+  // filmes populares "https://api.themoviedb.org/3/movie/popular?api_key=8426a1374a213b578abf04fbd0c08f8a&language=pt-BR"
+  // filmes top rated "https://api.themoviedb.org/3/movie/top_rated?api_key=8426a1374a213b578abf04fbd0c08f8a&language=pt-BR"
+  // serie populares "https://api.themoviedb.org/3/tv/popular?api_key=8426a1374a213b578abf04fbd0c08f8a&language=en-US"
+  // serie top rated "https://api.themoviedb.org/3/tv/top_rated?api_key=8426a1374a213b578abf04fbd0c08f8a&language=en-US"
+  // trending day https://api.themoviedb.org/3/trending/all/day?api_key=8426a1374a213b578abf04fbd0c08f8a
+  // trending week https://api.themoviedb.org/3/trending/all/week?api_key=8426a1374a213b578abf04fbd0c08f8a
+  useEffect(()=>{
+    fetch("")
+      .then((response)=>response.json())
+      .then((data)=>setFilmes(data.results))
+  },[])
+  console.log(filmes)
   return (
-    <>
-      <Header />
-    </>
+    <div>
+
+    </div>
   );
 }
 
